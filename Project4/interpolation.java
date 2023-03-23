65d7e4dcf36282177f281cd420bf48af7eb8626b
Ryan <98720555+rwlarson2024@users.noreply.github.com>
2023-03-22 23:27:21 -0700

    Add files via upload

---
+++ Project4/interpolation.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+public class interpolation 
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name of the file to be interpolated : "); 
+        String filename = input.nextLine();
+
+        File file = new File(filename);
+        Scanner fileScanner = new Scanner(file);
+
+        int n = fileScanner.nextInt();
+
+        double[][] userData = new double[n][2];
+        
+        for(int i = 0; i < n; i++)
+        {
+            userData[i][0] = fileScanner.nextDouble();
+        }
+        for( int i = 0; i < n;i ++ )
+        {
+            userData[i][1] = fileScanner.nextDouble();
+        }
+        fileScanner.close();
+        double [][] divDiff = new double[n][n];
+        for(int i = 0; i <n; i++)
+        {
+            divDiff[i][0] = userData[i][1];
+        }
+        for(int j = 1; j < n; j++)
+        {
+            for(int i = j; i < n; i++)
+            {
+                divDiff[i][j] = (divDiff[i][j-1] - divDiff[i-1][j-1] / userData[i][0] - userData[i-j][0]);
+            }
+        }
+
+        while(true)
+        {
+            System.out.print("enter a value to interpolate (q to quit)");
+            String input2 = input.next();
+            if(input2.equals("q"))
+            {
+                break;
+            }
+            double x = Double.parseDouble(input2);
+            double fx = divDiff[0][0];
+            double term = 1;
+
+            for(int i = 1; i < n ;i++)
+            {
+                term *= (x - userData[i-1][0]);
+                fx += divDiff[i][i] * term;
+            }
+            System.out.println("interpolated value at " + x +" is " + fx);
+        }
+        input.close();
+
+    }
+}
+    /*public static double[][] divided_Diff(double[][] data, int n)
+    {
+        double [][] divDiff = new double[n][n];
+        for(int i = 0; i <n; i++)
+        {
+            divDiff[i][0] = data[i][1];
+        }
+        for(int j = 1; j < n; j++)
+        {
+            for(int i = j; i < n; i++)
+            {
+                divDiff[i][j] = (divDiff[i][j-1] - divDiff[i-1][j-1] / data[i][0] -  data[i-j][0]);
+            }
+        }
+        return divDiff;
+    }
+}*/
